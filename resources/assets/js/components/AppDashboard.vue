e2df0ceb1c8b8a9a70eff3294da4a77d023415b4
joshuajazleung <joshuajazleung@gmail.com>
2017-07-05 21:54:56 -0700

    initial commit

---
+++ resources/assets/js/components/AppDashboard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <header-top />
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 col-md-4">
+          <navbox title="Request Files" message="Invite anyone to upload files to your Droplet, even if they don’t have an account." btnText="Create a file request" btnLink="#" type="pop" v-on:show="showRequestModal" />
+
+          <vodal :show="isRequestModal" animation="zoom" @hide="isRequestModal = false">
+              <div>A vue modal with animations.</div>
+          </vodal>
+        </div>
+        <div class="col-12 col-md-4">
+          <navbox title="Send Files" message="Upload files and share them with anyone, even if they don't have an account" btnText="Create a file sharing request" type="pop" btnLink="#" v-on:show="showSendModal" />
+
+          <vodal :show="isSendModal" animation="zoom" @hide="isSendModal = false">
+              <div>A vue modal with animations. Send</div>
+          </vodal>
+        </div>
+        <div class="col-12 col-md-4">
+          <navbox title="History" message="View all the requests you have made, and download the files of any request." btnText="View file request history" type="normal" btnLink="http://www.google.com" />
+        </div>
+      </div>  
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        isRequestModal: false,
+        isSendModal: false
+      }
+    },
+    methods: {
+      showRequestModal: function (e) {
+        this.isRequestModal = true
+      },
+      showSendModal: function (e) {
+        this.isSendModal = true
+      }
+    }
+  }
+</script>
+
+<style>
+  
+
+  .container-fluid {
+    margin-top: 2rem;
+  }
+</style>
---
+++ resources/assets/js/components/Navbox.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="card">
+  <div class="card-block">
+    <h3 class="card-title">{{ title }}</h3>
+    <p class="card-text">{{ message }}</p>
+    <a v-if="type === 'pop'" :href="btnLink" class="btn btn-primary" @click="showModal">{{ btnText }}</a>
+    <a v-else :href="btnLink" class="btn btn-primary">{{ btnText }}</a>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    props: ['title', 'message', 'btnText', 'btnLink', 'type'],
+    methods: {
+      showModal: function(e) {
+        e.preventDefault()
+        this.$emit('show')
+      }
+    }
+  }
+</script>
+
+<style></style>
